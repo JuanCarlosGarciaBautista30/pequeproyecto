7208ef92569d4f3fc6aacb08995f2033164f937f
Juan Carlos <JC_ORION_27@HOTMAIL.COM>
2023-07-17 19:34:10 -0600

    implementando  try exept

--- Aprendiendo.py
+++ Aprendiendo.py
@@ -1,24 +1,48 @@
-print('hola bienbenido') # se le da la bienvenida al usuario para convencerlo de que esta usando la app corrceta
-nombre=input('¿Cuál es tu nombre?')
+print('hola bienbenido\n') # se le da la bienvenida al usuario para convencerlo de que esta usando la app corrceta
+nombre=input('¿Cuál es tu nombre? \n')
 
-numero_1=int(input('ingresa una fecha: '))
-numero_aleatorio=int(input('ingresa otra fecha(la que gustes:)'))
+respuesta=0
 
-if numero_1 - numero_aleatorio >1:
+while True:
+  
+  try:
+      numero_1=int(input('ingresa una fecha la fecha en que te encuentras ahora: '))
+
+  except ValueError:
+      print('Herror, intenta de nuevo.')
+      continue  
+
+  try:
+     numero_aleatorio=int(input('ingresa otra fecha (la que gustes): '))
+
+  except ValueError:
+     print('Herror intenta de nuevo')   
+  
+  #validaciones con if
+  if numero_1 - numero_aleatorio >1:
     print(f'Han pasado {numero_1-numero_aleatorio} años desde {numero_aleatorio}')
+    
 
-elif numero_1 - numero_aleatorio <-1:
+  elif numero_1 - numero_aleatorio <-1:
     print(f'faltan {numero_aleatorio-numero_1} años para {numero_aleatorio}')
+    
 
-elif numero_1==numero_aleatorio:
+  elif numero_1==numero_aleatorio:
     print('estas en la misma fecha')
+    
 
-elif numero_1-numero_aleatorio ==1:
+  elif numero_1-numero_aleatorio ==1:
     print(f'ha pasado {numero_1-numero_aleatorio} año desde {numero_aleatorio}')
+    
 
-elif numero_1-numero_aleatorio == -1:
+  elif numero_1-numero_aleatorio == -1:
     print(f'falta {numero_aleatorio-numero_1} año para {numero_aleatorio}')
+    
 
-print('gracias por usar nuestra app.')
-print('espero regreses pronto')
+  
+  
+  
+ 
+
+
 
